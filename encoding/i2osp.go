09bf11716f5fa51e7892a46676811393f3762dd7
bytemare <{ID}+{username}@users.noreply.github.com>
2021-02-03 19:41:30 +0100

    proper I2OSP and OS2IP functions with some tests

--- encoding/i2osp.go
+++ encoding/i2osp.go
@@ -6,75 +6,101 @@ import (
 )
 
 const (
-	i2ospMaxInt8  = (1 << 8) - 1  // 255 - FF
-	i2ospMaxInt16 = (1 << 16) - 1 // 65535 - FFFF
-	i2ospMaxInt24 = (1 << 24) - 1 // FFFF FFFF
-	i2ospMaxInt32 = (1 << 32) - 1 // FFFF FFFF
-	// i2ospMaxInt64 = (1 << 64) - 1
+	i2ospMaxInt8  = (1 << 8) - 1  // FF = 255
+	i2ospMaxInt16 = (1 << 16) - 1 // FFFF = 65535
+	i2ospMaxInt24 = (1 << 24) - 1 // FF FFFF = 16777215
+	i2ospMaxInt32 = (1 << 32) - 1 // FFFF FFFF = 4294967295
 )
 
 var (
-	errI2OSPNegative = errors.New("forbidden negative value")
-	errI2OSPLarge    = errors.New("integer too large : > 2^32")
+	errInputNegative  = errors.New("negative input")
+	errInputLarge32   = errors.New("integer too large : > 2^32")
+	errInputLarge     = errors.New("input is too high for length")
+	errLengthNegative = errors.New("length is negative or 0")
+	errLengthTooBig   = errors.New("requested length is > 4")
+
+	errInputEmpty    = errors.New("nil or empty input")
+	errInputTooLarge = errors.New("input too large for integer")
 )
 
-// I2OSP1 Integer to Octet Stream Primitive on 1 byte.
-func I2OSP1(in uint) []byte {
-	if in > i2ospMaxInt8 {
-		panic("input is to big")
+func i2osp2(in, size, max uint) []byte {
+	if in > max {
+		panic(errInputLarge)
 	}
 
 	out := make([]byte, 2)
 
 	binary.BigEndian.PutUint16(out, uint16(in))
 
-	return out[1:]
+	return out[2-size:]
+}
+
+// I2OSP1 Integer to Octet Stream Primitive on 1 byte.
+func I2OSP1(in uint) []byte {
+	return i2osp2(in, 1, i2ospMaxInt8)
 }
 
 // I2OSP2 Integer to Octet Stream Primitive on 2 bytes.
 func I2OSP2(in uint) []byte {
-	if in > i2ospMaxInt16 {
-		panic("input is to big")
-	}
-
-	out := make([]byte, 2)
-
-	binary.BigEndian.PutUint16(out, uint16(in))
-
-	return out
+	return i2osp2(in, 2, i2ospMaxInt16)
 }
 
-// I2OSP Integer to Octet Stream Primitive on maximum 4 bytes.
-func I2OSP(in int) []byte {
-	if in < 0 {
-		panic(errI2OSPNegative)
+// I2OSP 32 bit Integer to Octet Stream Primitive on maximum 4 bytes.
+func I2OSP(input, length int) []byte {
+	if length <= 0 {
+		panic(errLengthNegative)
 	}
 
-	max := 4
-	out := make([]byte, max)
+	if length > 4 {
+		panic(errLengthTooBig)
+	}
 
-	switch ll := in; {
-	case ll <= i2ospMaxInt8:
+	out := make([]byte, 4)
+
+	switch in := input; {
+	case in < 0:
+		panic(errInputNegative)
+
+	case in >= 1<<(8*length):
+		panic(errInputLarge)
+
+	case in <= i2ospMaxInt8:
 		binary.BigEndian.PutUint16(out, uint16(in))
 
 		return out[1:2]
-	case ll <= i2ospMaxInt16:
+	case in <= i2ospMaxInt16:
 		binary.BigEndian.PutUint16(out, uint16(in))
 
 		return out[:2]
-	case ll <= i2ospMaxInt24:
-		binary.BigEndian.PutUint16(out, uint16(in))
+	case in <= i2ospMaxInt24:
+		binary.BigEndian.PutUint32(out, uint32(in))
 
 		return out[:3]
-	case ll <= i2ospMaxInt32:
+	case in <= i2ospMaxInt32:
 		binary.BigEndian.PutUint32(out, uint32(in))
 
 		return out
-	// case ll <= i2ospMaxInt64:
-	// 	binary.BigEndian.PutUint64(out, uint64(in))
-	// 	size := (int)(math.Log2(float64(in)/(float64(max))))
-	// 	return out[max-size:]
 	default:
-		panic(errI2OSPLarge)
+		panic(errInputLarge32)
+	}
+}
+
+// I2OSP Octet Stream to Integer Primitive on maximum 4 bytes / 32 bits.
+func OS2IP(input []byte) int {
+	switch length := len(input); {
+	case length == 0:
+		panic(errInputEmpty)
+	case length == 1:
+		b := []byte{0, input[0]}
+		return int(binary.BigEndian.Uint16(b))
+	case length == 2:
+		return int(binary.BigEndian.Uint16(input))
+	case length == 3:
+		b := append([]byte{0}, input...)
+		return int(binary.BigEndian.Uint16(b))
+	case length == 4:
+		return int(binary.BigEndian.Uint32(input))
+	default:
+		panic(errInputTooLarge)
 	}
 }
--- encoding/i2osp_test.go
+++ encoding/i2osp_test.go
@@ -1,39 +1,79 @@
 package encoding
 
 import (
+	"bytes"
 	"encoding/hex"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+type i2ospTest struct {
+	value   int
+	size    int
+	encoded []byte
+}
+
+var i2ospVectors = []i2ospTest{
+	{
+		0, 1, []byte{0},
+	},
+	{
+		1, 1, []byte{1},
+	},
+	{
+		255, 1, []byte{0xff},
+	},
+	{
+		256, 2, []byte{0x01, 0x00},
+	},
+	{
+		65535, 2, []byte{0xff, 0xff},
+	},
+}
+
 func TestI2OSP(t *testing.T) {
-	good := map[int]string{
-		0:     "00",
-		1:     "01",
-		255:   "ff",
-		256:   "0100",
-		65535: "ffff",
+	for i, v := range i2ospVectors {
+		t.Run(fmt.Sprintf("%d - %d - %v", v.value, v.size, v.encoded), func(t *testing.T) {
+			r := I2OSP(v.value, v.size)
+
+			if !bytes.Equal(r, v.encoded) {
+				t.Fatalf("invalid encoding for %d. Expected '%s', got '%v'", i, hex.EncodeToString(v.encoded), hex.EncodeToString(r))
+			}
+
+			value := OS2IP(v.encoded)
+			if v.value != value {
+				t.Fatalf("invalid decoding for %d. Expected %d, got %d", i, v.value, value)
+			}
+		})
 	}
 
-	for k, v := range good {
-		r := I2OSP(k)
+	length := -1
+	assert.PanicsWithError(t, errLengthNegative.Error(), func() {
+		_ = I2OSP(1, length)
+	}, "expected panic with negative length")
 
-		if hex.EncodeToString(r) != v {
-			t.Fatalf("invalid encoding for %d. Expected '%s', got '%v'", k, v, hex.EncodeToString(r))
-		}
-	}
+	length = 0
+	assert.PanicsWithError(t, errLengthNegative.Error(), func() {
+		_ = I2OSP(1, length)
+	}, "expected panic with 0 length")
+
+	length = 5
+	assert.PanicsWithError(t, errLengthTooBig.Error(), func() {
+		_ = I2OSP(1, length)
+	}, "expected panic with length too high")
 
 	negative := -1
-	tooLarge := 1 << 32
+	assert.PanicsWithError(t, errInputNegative.Error(), func() {
+		_ = I2OSP(negative, 4)
+	}, "expected panic with negative input")
 
-	assert.PanicsWithError(t, errI2OSPNegative.Error(), func() {
-		_ = I2OSP(negative)
-	}, "expected panic with negative value")
-
-	assert.PanicsWithError(t, errI2OSPLarge.Error(), func() {
-		_ = I2OSP(tooLarge)
-	}, "expected panic with big value")
+	tooLarge := 1 << 8
+	length = 1
+	assert.PanicsWithError(t, errInputLarge.Error(), func() {
+		_ = I2OSP(tooLarge, length)
+	}, "expected panic with exceeding value for the length")
 
 	lengths := map[int]int{
 		100:           1,
@@ -43,7 +83,7 @@ func TestI2OSP(t *testing.T) {
 	}
 
 	for k, v := range lengths {
-		r := I2OSP(k)
+		r := I2OSP(k, v)
 
 		if len(r) != v {
 			t.Fatalf("invalid length for %d. Expected '%d', got '%d' (%v)", k, v, len(r), r)
