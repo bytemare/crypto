b3652d863999b5351997de3e590a9336bb1b7e80
bytemare <3641580+bytemare@users.noreply.github.com>
2021-08-13 17:48:30 +0200

    update doc

--- doc.go
+++ doc.go
@@ -13,13 +13,13 @@ It enables using one interface for a variety of underlying primitives, and witho
 
 Available interfaces are:
 
-- HashToGroup: implementing hashing of arbitrary strings into prime-order groups, after hash-to-curve.
+- Group: implementing hashing of arbitrary strings into prime-order groups, after hash-to-curve.
 
 - Hash: interface to hashing primitives and exposing common functions such as hashing, hmac, HKDF, and expand-only HKDF.
 
 - MHF: for memory hard function, a.k.a. password key derivation functions.
 
-- Encoding: for encoding and decoding to and from different formats.
+- Signature: for public key signatures.
 
 */
 package crypto
